e9c6fa7faa4b59373a58aa449fa1aa3b0658c6ee
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-14 00:09:53 +0200

    datastructure/struct.c: fix file

--- datastructure/struct.c
+++ datastructure/struct.c
@@ -1,16 +1,30 @@
+/* Copyright (C) 2020  David Arroyo Menéndez
+   Author: David Arroyo Menéndez <davidam@gnu.org>
+   Maintainer: David Arroyo Menéndez <davidam@gnu.orgxs>
+   This file is software with freedom of class; you can
+   redistribute it and/or modify it  only for the working
+   class under the terms of the Working Class License
+   published by David Arroyo Menéndez.
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   Working Class License for more details.
+   You should have received a copy of the Working Class License along
+   with c-examples; see the file COPYING. If not, see
+   <https://github.com/davidam/workingclasslicense>.
+*/
+
 #include <stdio.h>
  
 struct product
 {
-    char name[30];
-    int stock;
-    float price, dis;
-};
+    char name[30]
+    int stock
+    float price, dis
+}
  
 void main()
 {
-     struct product p1 ={"GNU Emacs", 35,298.56, 2.32};
-     //     clrscr();
-     printf("Name=%s,\nStock=%d,\nPrice=$%.2f,\nDiscount=%.2f%.", p1.name, p1.stock, p1.price,p1.dis);
-     //getch();
+     struct product p1 = {"GNU Emacs", 35, 298.56, 2.32}
+     printf("Name=%s,\nStock=%d,\nPrice=$%.2f,\nDiscount=%.2f%.", p1.name, p1.stock, p1.price, p1.dis)
 }
