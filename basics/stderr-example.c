2fdce43c656dabd9eae58430707ea37e38dcdb1c
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-14 16:55:52 +0200

    basics/stderr-example.c basics/stderr-example2.c: creation

---
+++ basics/stderr-example.c
@@ -0,0 +1,35 @@
+/* Copyright (C) 2020  David Arroyo Menéndez
+   Author: David Arroyo Menéndez <davidam@libresoft>
+   Maintainer: David Arroyo Menéndez <davidam@libresoft>
+   This file is software with freedom of class; you can
+   redistribute it and/or modify it  only for the working
+   class under the terms of the Working Class License
+   published by David Arroyo Menéndez.
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   Working Class License for more details.
+   You should have received a copy of the Working Class License along
+   with c-examples; see the file COPYING. If not, see
+   <https://github.com/davidam/workingclasslicense>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+
+   int dividend = 20;
+   int divisor = 0;
+   int quotient;
+ 
+   if( divisor == 0){
+      fprintf(stderr, "Division by zero! Exiting...\n");
+      exit(-1);
+   }
+   
+   quotient = dividend / divisor;
+   fprintf(stderr, "Value of quotient : %d\n", quotient );
+
+   exit(0);
+}
