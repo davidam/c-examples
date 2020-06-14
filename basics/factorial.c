61245864c3eb83ce48ba8e25e60f3d5d4e791916
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-14 16:59:19 +0200

    basics/factorial.c: creation

---
+++ basics/factorial.c
@@ -0,0 +1,31 @@
+/* Copyright (C) 2020  David Arroyo Menéndez
+   Author: David Arroyo Menéndez <davidam@libresoft>
+   Maintainer: David Arroyo Menéndez <davidam@libresoft>
+   This file is software with freedom of class; you can
+   redistribute it and/or modify it  only for the working
+   class under the terms of the Working Class License
+   published by David Arroyo Menéndez.
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   Working Class License for more details.
+   You should have received a copy of the Working Class License along
+   with c-examples; see the file COPYING. If not, see
+   <https://github.com/davidam/workingclasslicense>.
+*/
+
+#include <stdio.h>
+
+int factorial(unsigned int i) {
+
+   if(i <= 1) {
+      return 1;
+   }
+   return i * factorial(i - 1);
+}
+
+int  main() {
+   int i = 12;
+   printf("Factorial of %d is %d\n", i, factorial(i));
+   return 0;
+}
