a66a2bc378ec0ca880d04c1b23929311f4dc395b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-14 14:36:40 +0200

    basics/fibonacci.c: creation

---
+++ basics/fibonacci.c
@@ -0,0 +1,40 @@
+/* Copyright (C) 2020  David Arroyo Menéndez
+   Author: David Arroyo Menéndez <davidam@gnu.org>
+   Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+   This file is software with freedom of class; you can
+   redistribute it and/or modify it  only for the working
+   class under the terms of the Working Class License
+   published by David Arroyo Menéndez.
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   Working Class License for more details.
+   You should have received a copy of the Working Class License along
+   with c-examples; see the file COPYING. If not, see
+   <https://github.com/davidam/workingclasslicense>.
+*/
+
+#include <stdio.h>
+
+int fibonacci(int i) {
+
+   if(i == 0) {
+      return 0;
+   }
+	
+   if(i == 1) {
+      return 1;
+   }
+   return fibonacci(i-1) + fibonacci(i-2);
+}
+
+int  main() {
+
+   int i;
+	
+   for (i = 0; i < 10; i++) {
+      printf("%d\t\n", fibonacci(i));
+   }
+	
+   return 0;
+}
