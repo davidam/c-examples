64231f65649a2f208a1c242b01210e2a847d5167
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-14 17:48:43 +0200

    basics/scanf-example.c: creation

---
+++ basics/scanf-example.c
@@ -0,0 +1,30 @@
+/* Copyright (C) 2020  David Arroyo Menéndez
+   Author: David Arroyo Menéndez <davidam@libresoft>
+   Maintainer: David Arroyo Menéndez <davidam@libresoft>
+   This file is software with freedom of class; you can
+   redistribute it and/or modify it  only for the working
+   class under the terms of the Working Class License
+   published by David Arroyo Menéndez.
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   Working Class License for more details.
+   You should have received a copy of the Working Class License along
+   with c-examples; see the file COPYING. If not, see
+   <https://github.com/davidam/workingclasslicense>.
+*/
+
+
+/* Uso de la sentencia scanf() */
+
+#include <stdio.h>
+
+void main() /* Solicita dos datos */
+{
+  char nombre[10];
+  int edad;
+  printf("Introduce tu nombre: ");
+  scanf("%s",nombre);
+  printf("Introduce tu edad: ");
+  scanf("%d",&edad);
+}
